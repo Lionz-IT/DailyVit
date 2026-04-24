a9a8b4ed713e3e4eda49d11f24ac4caf9befff2e
Rafif Ahmad Yudhistira <raymlg2005@gmail.com>
2026-04-24 18:33:47 +0700

    delete unused import useLocation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Dashboard } from './pages/Dashboard';
 import { History } from './pages/History';
